7f15238e5d578d587694c2bb40d1edc5b4c276e9
zer0 <max.hasselwander@wiss-edu.ch>
2024-10-29 16:19:55 +0100

    added spring boot Project295

---
+++ src/main/java/ch/zero/Project295/Project295Application.java
@@ -0,0 +1,13 @@
+package ch.zero.Project295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project295Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project295Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zero/Project295/Project295ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zero.Project295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
